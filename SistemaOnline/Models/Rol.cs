91281d153376d16d3decc47b44304305bdb96140
Diego Fabian <dfabiansevillano@gmail.com>
2026-05-17 18:59:53 -0500

    feat: add User and Role models

---
+++ SistemaOnline/Models/Rol.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaOnline.Models
+{
+    public class Rol
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID_Rol { get; set; }
+
+        [StringLength(50)]
+        public string Nombre_Rol { get; set; }
+
+        [StringLength(150)]
+        public string Descripcion { get; set; }
+
+        // Un rol tiene muchos usuarios
+        public ICollection<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ SistemaOnline/Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaOnline.Models
+{
+    public class Usuario
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID_Usuario { get; set; }
+
+        [StringLength(50)]
+        public string Nombre_Usuario { get; set; }
+
+        [StringLength(100)] 
+        public string Email { get; set; }
+
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public bool? Estado { get; set; }
+
+        // Relacion con Rol
+        public int ID_Rol { get; set; }
+        public Rol Rol { get; set; }
+    }
+}

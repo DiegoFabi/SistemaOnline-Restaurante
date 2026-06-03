9334e252718f56efd1e46a775292c59a6f323e93
Diego Fabian <dfabiansevillano@gmail.com>
2026-06-03 12:32:04 -0500

    feat: agregar LoginController de autenticación y registro de usuarios

---
+++ SistemaOnline/Controllers/LoginController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaOnline.Data;
+using SistemaOnline.Models;
+using SistemaOnline.ViewModels;
+using System.Security.Claims;
+
+namespace SistemaOnline.Controllers
+{
+    [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+    public class LoginController : Controller
+    {
+        private readonly APPDBContext _dbcontext;
+        public LoginController(APPDBContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            ViewBag.Rol = _dbcontext.Roles.ToList();
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Registro(UserVM modelo)
+        {
+            if (modelo.Password != modelo.RepeatPassword)
+            {
+                ViewData["msg"] = "La contraseñas no coinciden, escribe denuevo";
+                ViewBag.Rol = _dbcontext.Roles.ToList();
+                return View();
+            }
+            Usuario user = new Usuario()
+            {
+                Nombre_Usuario = modelo.Username,
+                Email = modelo.Email,
+                Password = modelo.Password,
+                ID_Rol = modelo.idRol
+            };
+            await _dbcontext.Usuarios.AddAsync(user);
+            await _dbcontext.SaveChangesAsync();
+            if (user.ID_Usuario != 0) return RedirectToAction("Login", "Login");
+            ViewData["msg"] = "El usuario no se creo";
+            ViewBag.Roles = _dbcontext.Roles.ToList();
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM modelo)
+        {
+            Usuario? existe = await _dbcontext.Usuarios.Include(u => u.Rol).FirstOrDefaultAsync(
+                u => u.Email == modelo.Email &&
+                u.Password == modelo.Password);
+
+            if (existe == null)
+            {
+                ViewData["msg"] = "El usuario no exise o esta mal escrito";
+                return View();
+            }
+            //Claims
+            var claim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, existe.Nombre_Usuario),
+                new Claim(ClaimTypes.Email, existe.Email),
+                new Claim(ClaimTypes.Role, existe.Password),
+                new Claim("idUser", existe.ID_Usuario.ToString())
+            };
+            var claimsIdentity = new ClaimsIdentity(claim, CookieAuthenticationDefaults.AuthenticationScheme);
+            var propiedad = new AuthenticationProperties()
+            {
+                AllowRefresh = true,
+                IsPersistent = true
+            };
+            await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            propiedad
+            );
+            //Sesiones por rol
+            HttpContext.Session.SetString("User", existe.Nombre_Usuario);
+            HttpContext.Session.SetString("Rol", existe.Rol.Nombre_Rol);
+            HttpContext.Session.SetInt32("idUser", existe.ID_Rol);
+            switch (existe.Rol.Nombre_Rol)
+            {
+                case "Administrador":
+                    return RedirectToAction("Admin", "Login");
+                case "Cocinero":
+                    return RedirectToAction("Cocinero", "Login");
+                case "Mesero":
+                    return RedirectToAction("Mesero", "Login");
+                case "Cajero":
+                    return RedirectToAction("Cajero", "Login");
+                default:
+                    return RedirectToAction("Cliente", "Login");
+            }
+        }
+        [HttpGet]
+        public IActionResult Administrador()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cocinero()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Mesero()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Cajero()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Cliente()
+        {
+            return View();
+        }
+
+    }
+}

d0db16ac73478f322cde5f13197d6881b48ef6c5
Diego Fabian <dfabiansevillano@gmail.com>
2026-06-13 19:17:31 -0500

    refactor: update table with new foreign key relations

--- SistemaOnline/Models/Empleado_Turno.cs
+++ SistemaOnline/Models/Empleado_Turno.cs
@@ -1,11 +1,16 @@
-﻿namespace SistemaOnline.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaOnline.Models
 {
     public class Empleado_Turno
     {
         // tabla intermedia para relacion muchos a muchos entre Empleado y Turno
+        [Key, Column(Order = 0)]
         public int ID_Turno { get; set; }
         public Turno Turno { get; set; }
 
+        [Key, Column(Order = 1)]
         public int ID_Empleado { get; set; }
         public Empleado Empleado { get; set; }
     }

63b1e6b760ec3d3bc7dbaa2eb2941fe3c9b97ec2
Diego Fabian <dfabiansevillano@gmail.com>
2026-06-26 13:26:59 -0500

    feat: agregar validaciones a nivel global

--- SistemaOnline/Program.cs
+++ SistemaOnline/Program.cs
@@ -1,11 +1,31 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using SistemaOnline.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(options =>
+{
+    options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+        value => $"El valor '{value}' no es válido.");
+    options.ModelBindingMessageProvider.SetValueMustNotBeNullAccessor(
+        value => "Este campo es obligatorio.");
+    options.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor(
+        value => $"Se requiere un valor para '{value}'.");
+    options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+        (value, field) => $"El valor '{value}' no es válido para '{field}'.");
+    options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+        () => "Este campo es obligatorio.");
+    options.ModelBindingMessageProvider.SetNonPropertyAttemptedValueIsInvalidAccessor(
+        value => $"El valor '{value}' no es válido.");
+    options.ModelBindingMessageProvider.SetNonPropertyUnknownValueIsInvalidAccessor(
+        () => "El valor enviado no es válido.");
+    options.ModelBindingMessageProvider.SetUnknownValueIsInvalidAccessor(
+        field => $"El valor enviado para '{field}' no es válido.");
+});
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
@@ -26,6 +46,15 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+var spanishCulture = new CultureInfo("es-ES");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(spanishCulture, spanishCulture),
+    SupportedCultures = new List<CultureInfo> { spanishCulture },
+    SupportedUICultures = new List<CultureInfo> { spanishCulture }
+};
+app.UseRequestLocalization(localizationOptions);
 
 app.UseStaticFiles();
 app.UseRouting();

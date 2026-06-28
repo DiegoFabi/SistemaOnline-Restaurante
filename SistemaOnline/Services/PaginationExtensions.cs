0cc37ee4a7fab06fa929d85ff0dcc487552c7f4c
Diego Fabian <dfabiansevillano@gmail.com>
2026-06-27 19:06:43 -0500

    feat: agregar paginacion a todos los CRUD

---
+++ SistemaOnline/Services/PaginationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaOnline.ViewModels;
+
+namespace SistemaOnline.Services
+{
+    public static class PaginationExtensions
+    {
+        public const int DefaultPageSize = 10;
+        public static readonly int[] TamanosPaginaPermitidos = { 10, 20, 30, 50 };
+
+        public static async Task<PagedResult<T>> ToPagedListAsync<T>(this IQueryable<T> source, int page, int pageSize)
+        {
+            if (page < 1) page = 1;
+            if (!TamanosPaginaPermitidos.Contains(pageSize)) pageSize = DefaultPageSize;
+
+            int totalCount = await source.CountAsync();
+            int totalPages = pageSize <= 0 ? 1 : (int)Math.Ceiling(totalCount / (double)pageSize);
+            if (totalPages < 1) totalPages = 1;
+            if (page > totalPages) page = totalPages;
+
+            var items = await source.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                Page = page,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
---
+++ SistemaOnline/ViewModels/PagedResults.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaOnline.ViewModels
+{
+    public class PagedResult<T>
+    {
+        public List<T> Items { get; set; } = new();
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalCount { get; set; }
+        public int TotalPages => PageSize <= 0 ? 1 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
